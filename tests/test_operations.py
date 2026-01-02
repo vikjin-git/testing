796cff1ba1dd71811f70dc3a6020d3e8c3d754b3
Vikas <vkasmail@gmail.com>
2026-01-02 11:39:43 -0800

    update case2

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,7 +1,7 @@
 from src.math_operations import add, substract, dotfun
 
 def test_add():
-    assert add(2,5)==9
+    assert add(2,5)==7
     assert substract(10,2)==8
     assert dotfun(2,6)==12
 
